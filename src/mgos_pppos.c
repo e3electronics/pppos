a99e726163272af630bf481d706f3209c94a6a77
Artemio <artemioev@gmail.com>
2021-03-13 16:53:34 -0400

    update

--- src/mgos_pppos.c
+++ src/mgos_pppos.c
@@ -556,7 +556,7 @@ static void mgos_pppos_dispatch_once(struct mgos_pppos_data *pd) {
     }
     case PPPOS_RESET: {
       const double now = mgos_uptime();
- LOG(LL_INFO, ("Reset vales GPIO %d  State: %d ",pd->cfg->rst_gpio,  pd->cfg->rst_act);
+ LOG(LL_INFO, ("Reset vales GPIO %d  State: %d ",pd->cfg->rst_gpio,  pd->cfg->rst_act));
       
       mgos_gpio_setup_output(pd->cfg->rst_gpio, pd->cfg->rst_act);
       pd->deadline = now + (pd->cfg->rst_hold_ms / 1000.0);
